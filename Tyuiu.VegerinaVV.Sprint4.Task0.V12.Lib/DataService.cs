a8c89b8c32e5b2c010adac95c5673862a61c55ba
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-04 17:57:53 +0500

    Add project files.

---
+++ Tyuiu.VegerinaVV.Sprint4.Task0.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VegerinaVV.Sprint4.Task0.V12.Lib
+{
+    public class DataService : ISprint4Task0V12
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int res = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    res *= array[i];
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint4.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VegerinaVV.Sprint4.Task0.V12.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint4.Task0.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultEvenArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 2 };
+            int res = ds.GetMultEvenArrEl(numsArray);
+            int wait = 768;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
