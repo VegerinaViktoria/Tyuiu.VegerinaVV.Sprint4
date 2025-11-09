4daadb5bd23a400dd64f7247070598c8c9154f0d
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-09 15:41:45 +0500

    Task3

---
+++ Tyuiu.VegerinaVV.Sprint4.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.VegerinaVV.Sprint4.Task3.V27.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint4.Task3.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 3, 4, 5, 6, 7 },
+                                          { 4, 5, 6, 7, 4 },
+                                          { 3, 3, 4, 5, 5 },
+                                          { 4, 4, 6, 6, 3 },
+                                          { 3, 4, 6, 5, 7 } };
+
+            int res = ds.Calculate(mas2);
+            int wait = 12;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint4.Task3.V27/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.VegerinaVV.Sprint4.Task3.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* статическими значениями в диапазоне от 3 до 7. Подсчитайте количество   *");
+        Console.WriteLine("* четных элементов во всем массиве.                                       *");
+        Console.WriteLine("*                                                                         *");
+
+        int[,] mtrx = new int[5, 5] { { 4, 3, 5, 5, 3 },
+                                      { 6, 7, 4, 4, 3 },
+                                      { 3, 3, 7, 3, 6 },
+                                      { 3, 4, 3, 7, 7 },
+                                      { 3, 5, 6, 3, 6 }};
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(mtrx);
+
+        Console.WriteLine("Количество четных элементов массива = " + res);
+        Console.ReadKey();
+    }
+}
