6b7db5c0bd5ebcb28cd67c0ed7412cbd1f727262
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-09 19:34:22 +0500

    Task6

---
+++ Tyuiu.VegerinaVV.Sprint4.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VegerinaVV.Sprint4.Task6.V14.Lib
+{
+    public class DataService : ISprint4Task6V14
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length > 3);
+            return mas;
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint4.Task6.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.VegerinaVV.Sprint4.Task6.V14.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Класс Array                                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан строковый массив данных [\"Январь\", \"Февраль\", \"Март\",         *");
+        Console.WriteLine("* \"Апрель\", \"Май\", \"Июнь\", \"Июль\"], используя класс Array,        *");
+        Console.WriteLine("* выведите элементы массива, длина которых больше 3 символа.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        var month=new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль" };
+
+        Console.WriteLine("Исходный массив:");
+        for (int i=0; i <= month.Length - 1; i++)
+        {
+            Console.WriteLine(month[i]);
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Элементы, длина которых больше 3:");
+
+        string[] res = ds.Calculate(month);
+        var str = string.Join(",", res);
+        Console.WriteLine(str);
+        Console.ReadKey();
+    }
+}
